4dfbf707115da3ce1653576c392d6640985e349f
AhmedFelic <ahmedfelic1@gmail.com>
2022-01-08 19:20:22 +0100

    tabs

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// Za sad nista od ovog koda nije moje, ali razumijem vecinu
+
 let loader_wrapper = document.querySelector('.loader_wrapper');
 window.addEventListener('load', function(){
     loader_wrapper.style.display='none';
 });
+
+let = tabs = document.querySelectorAll('.tabs_toggle'),
+    contents = document.querySelectorAll('.tabs_content');
+   
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            contents.forEach((content)=>{
+                content.classList.remove('is-active');
+            });
+            tabs.forEach((tab)=>{
+                tab.classList.remove('is-active');
+            });
+            contents[index].classList.add('is-active');
+            tabs[index].classList.add('is-active');
+        });
+    });
